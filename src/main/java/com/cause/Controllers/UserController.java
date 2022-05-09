02991b567eb3d8f0f23183aad390a8136d0be4ba
Tanuj Sharma <54256549+tanuj1811@users.noreply.github.com>
2022-05-09 19:15:43 +0530

    Update UserController.java

--- src/main/java/com/cause/Controllers/UserController.java
+++ src/main/java/com/cause/Controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://cause-one.vercel.app/")
 @RestController
 @RequestMapping("api/users")
 @AllArgsConstructor
