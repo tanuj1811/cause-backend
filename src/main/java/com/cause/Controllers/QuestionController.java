c00fc519d5b254750d667ee6214d3e1da54d5c39
Tanuj Sharma <54256549+tanuj1811@users.noreply.github.com>
2022-05-09 19:15:23 +0530

    Update QuestionController.java

--- src/main/java/com/cause/Controllers/QuestionController.java
+++ src/main/java/com/cause/Controllers/QuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://cause-one.vercel.app/")
 @RestController
 @RequestMapping("api/queries")
 @AllArgsConstructor
